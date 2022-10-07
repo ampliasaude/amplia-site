ae58311300c75d5545206c20777cb741f13f7e56
renato-mauro <renato.mauro@gmail.com>
2022-10-07 15:02:33 -0300

    atualização do mapa

--- pages/mapa/index.js
+++ pages/mapa/index.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
+import {Runtime, Inspector} from "@observablehq/runtime";
+import notebook from "b335ec6d8256f69f";
+import { adjustObservableWidth } from "../../components/utils";
 
 function Mapa() {
-    return(<>
-        <h1>Mapa - Em Construção</h1>
-    </>)
+
+  const mainWindowRef = useRef();
+  const allStylesRef = useRef();
+  const stylesRef = useRef();
+
+  useEffect(() => {
+    const runtime = new Runtime();
+    let main = runtime.module(notebook, name => {
+      if (name === "mainWindow") return new Inspector(mainWindowRef.current);
+      if (name === "allStyles") return new Inspector(allStylesRef.current);
+      if (name === "styles") return new Inspector(stylesRef.current);
+      return ["clearMainWindow","panel","populate","afterInitialLayout","populateMapa","viewof dorling","viewof colorSelect", "color", "dorlingCircleConf","storageUpdate"].includes(name);
+    });
+    adjustObservableWidth(mainWindowRef, main);
+    return () => runtime.dispose();
+  }, []);
+
+  return (
+    <>
+      <div ref={mainWindowRef} />
+      <div ref={allStylesRef} />
+      <div ref={stylesRef} />
+    </>
+  );
 }
 
 Mapa.tipo = "mapa";
 
 export default Mapa;
-  
